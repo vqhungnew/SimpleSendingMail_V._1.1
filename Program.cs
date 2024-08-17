c26a3b73a81a5511ac84f227f2087c9a4099336f
PeterVuQuocHung <peter.vuquochung@gmail.com>
2024-08-17 20:45:38 +0700

    Change all

--- Program.cs
+++ Program.cs
@@ -1,25 +1,63 @@
-﻿using System;
+﻿//REF:https://www.tutorialsteacher.com/articles/send-emails-in-csharp
+
+using System;
+using System.Net;
 using System.Net.Mail;
-using System.Threading.Tasks;
-using MailKit.Net.Smtp;
-using MimeKit;
+using System.Reflection.Metadata.Ecma335;
 
-public class EmailService
+class Program
 {
-    public async Task SendEmailAsync(string toEmail, string subject, string body)
+    static void Main(string[] args)
     {
-        var email = new MimeMessage();
-        email.From.Add(new MailboxAddress("Your Name", "your-email@example.com"));
-        email.To.Add(new MailboxAddress("", toEmail));
-        email.Subject = subject;
+        MailMessage mailMessage = new MailMessage();
+        mailMessage.From = new MailAddress("vaa_laptrinhmang@outlook.com");
+        Console.WriteLine("This is a SIMPLE and TEST program to send email by C#\n");
+        Console.WriteLine("To use this app, YOU MUST register an EMAIL from website OPUTLOOK.COM. You email should look like xxxx@OUTLOOK.COM\n");
+        //Get Receiver'e email
+        Console.WriteLine("\n Enter email of Receiver:");
+        string recvEmail = Console.ReadLine();
+        mailMessage.To.Add(recvEmail);
+        mailMessage.Subject = "Subject";
+        mailMessage.Body = "This is test email";
 
-        var builder = new BodyBuilder { HtmlBody = body };
-        email.Body = builder.ToMessageBody();
+        SmtpClient smtpClient = new SmtpClient();
+        smtpClient.Host = "eas.outlook.com";
+        smtpClient.Port = 587;
+        smtpClient.UseDefaultCredentials = false;
 
-        using var smtp = new SmtpClient();
-        await smtp.ConnectAsync("smtp.example.com", 587, false);
-        await smtp.AuthenticateAsync("your-email@example.com", "your-password");
-        await smtp.SendAsync(email);
-        await smtp.DisconnectAsync(true);
+
+        //Get password from keyboard
+        string emailPass = getPasswordFromUser();
+        
+        smtpClient.Credentials = new NetworkCredential("vaa_laptrinhmang@outlook.com", emailPass);
+        smtpClient.EnableSsl = true;
+
+        try
+        {
+            smtpClient.Send(mailMessage);
+            Console.WriteLine("Email Sent Successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+
+    //make password INVISIBLE on the screen
+    //https://stackoverflow.com/questions/23433980/c-sharp-console-hide-the-input-from-console-window-while-typing
+
+    public static string getPasswordFromUser()
+    {
+        System.Console.Write("\nEnter your password of yor email ENDING by ENTER key: ");
+        string password = null;
+        while (true)
+        {
+            var key = System.Console.ReadKey(true);
+            if (key.Key == ConsoleKey.Enter)
+                break;
+            password += key.KeyChar;
+            // Console.WriteLine(password);
+        }
+        return password;
     }
 }
